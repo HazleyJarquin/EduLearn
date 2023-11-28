6744103b84058eb9a3e3d3739f7e209cbcbbc870
HazGallo <hazleyjarquin03@gmail.com>
2023-11-28 14:25:46 -0600

    Update of api

---
+++ src/api/apiEduLearn.ts
@@ -0,0 +1,31 @@
+export const imgs = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Ciencia",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Tecnologia",
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Arte",
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1543109740-4bdb38fda756?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Lengua Extranjera",
+  },
+  {
+    id: 5,
+    url: "https://images.unsplash.com/photo-1635372722656-389f87a941b7?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Matematica",
+  },
+  //   { id: 6, url: ImgBody, title: "Matematica" },
+  //   { id: 7, url: ImgBody, title: "Matematica" },
+  //   { id: 8, url: ImgBody, title: "Matematica" },
+  //   { id: 9, url: ImgBody, title: "Matematica" },
+];
